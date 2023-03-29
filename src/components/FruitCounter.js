f3101c58b4c4c5ddbcc301000210e0dfaf70727a
Mehmoli <meagulec@gmail.com>
2023-03-29 18:01:42 +0200

    regel 7 gewijzigd van (counter -1) naar (curr => curr -1) en regel 11 gewijzigd  dan +

--- src/components/FruitCounter.js
+++ src/components/FruitCounter.js
@@ -4,11 +4,11 @@ import Button from './Button'
 function FruitCounter({setCounter, counter}) {
     return (
         <>
-            <Button buttonType="button" isDisabled={counter < 1} handleClick={() => setCounter(counter - 1)}>
+            <Button buttonType="button" isDisabled={counter < 1} handleClick={() => setCounter(curr =>curr  - 1)}>
                 -
             </Button>
             {counter}
-            <Button buttonType="button" isDisabled={false} handleClick={() => setCounter(counter + 1)}>
+            <Button buttonType="button" isDisabled={false} handleClick={() => setCounter(curr =>curr + 1)}>
                 +
             </Button>
         </>
